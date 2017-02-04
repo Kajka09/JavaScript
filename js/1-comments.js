1ca96bf9becdc5e2a7304c723e6dbf3b90cce70f
Kajka09 <kajkastefaniak@gmail.com>
2017-02-04 09:48:09 +0100

    zmienne

---
+++ js/1-comments.js
@@ -0,0 +1,7 @@
+'use strict';
+
+console.log('Akademia 108');
+
+//komentarz liniowy CTRL +/ /
+
+/* komentarz blokowy */
---
+++ js/2-variables.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var imie;
+var imieDamskie = "Kaja";
+var kolor = 'red';
+var wiek = 37;
+
+imie = "Sergio";
+
+imie = imieDamskie;
+
+
+
+console.log(imie);
+console.log(kolor);
+console.log(wiek);
