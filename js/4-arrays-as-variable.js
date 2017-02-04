0f20e540d4ca04c0c28d876c66d6269c32468054
Kajka09 <kajkastefaniak@gmail.com>
2017-02-04 10:45:37 +0100

    tablice

---
+++ js/4-arrays-as-variable.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/*
+
+Tablice - Arrays
+
+*/
+
+var imiona = new Array ();
+var imionaSteudentow = new Array (9);
+var imionaChlopakow = new Array ('Mateusz', 'Michał', 'Krzysiek', 'Sergio');
+
+imionaSteudentow[2] = "Przemek";
+
+console.log(imiona);
+console.log(imionaSteudentow);
+console.log(imionaChlopakow);
+
+
+var wiek = [];
+var wiekStudentow = [20,25,28];
+
+console.log(wiek);
+console.log(wiekStudentow);
